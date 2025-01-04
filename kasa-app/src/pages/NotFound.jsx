1500d618711b6b3b6acd11b7307b58fbc9e4d148
Rogue06 <111644419+Rogue06@users.noreply.github.com>
2025-01-04 12:45:39 +0100

    feat(404): Style et mise en page de la page d'erreur

--- kasa-app/src/pages/NotFound.jsx
+++ kasa-app/src/pages/NotFound.jsx
@@ -1,12 +1,16 @@
 // src/pages/NotFound.jsx
+import { Link } from 'react-router-dom';
+import '../styles/NotFound.scss';
+
 function NotFound() {
     return (
-      <div>
-        <h1>Erreur 404</h1>
-        <p>La page que vous recherchez n’existe pas.</p>
-      </div>
+        <div className="error-container">
+            <h1>404</h1>
+            <p>Oups! La page que vous demandez n&apos;existe pas.</p>
+            <Link to="/" className="error-link">Retourner sur la page d&apos;accueil</Link>
+        </div>
     );
-  }
-  
-  export default NotFound;
+}
+
+export default NotFound;
   
